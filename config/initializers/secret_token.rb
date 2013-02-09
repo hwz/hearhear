18a6bbac7ead3e3b2600eee98033d0704ef762b2
Helen Zeng <helen.w.zeng@gmail.com>
2013-02-09 11:45:17 -0800

    added initial things

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hearhear::Application.config.secret_token = '46b1043d5cd05fb83f7e3e97e16dd3508db61b54e097820658ec198a3ae0ce397c1ca503516794c1a8f18851e1697b60ec7671351e7b935b97fed3500227a26a'
